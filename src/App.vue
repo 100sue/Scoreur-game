d546dba2335ee2e21c897a7ae2c0d0581820a641
appert <scoreur@gmail.com>
2022-10-10 09:46:55 +0200

    fin de l'appli

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ const outcomes = {
   },
 
 }
-const winPercentage = computed(()=>{
+const winPercentage = computed(() => {
   const total = wins.value + drawns.value + losses.value
   return total ? (wins.value / total) * 100 : 0
 })
@@ -57,23 +57,23 @@ const play = c => {
 
 const SaveGame = () => {
   localStorage.setItem('wins', wins.value)
-  localStorage.setItem('draws', draws.value)
+  localStorage.setItem('drawns', drawns.value)
   localStorage.setItem('losses', losses.value)
 }
 
 const LoadGame = () => {
-  wins.value = localStorage.getItem('wins')
-  draws.value = localStorage.getItem('draws')
-  losses.value = localStorage.getItem('losses')
+  wins.value = parseInt(localStorage.getItem('wins')) || 0
+  drawns.value = parseInt(localStorage.getItem('drawns')) || 0
+  losses.value = parseInt(localStorage.getItem('losses')) || 0
 }
 
-const ResetGame = () => {
+const ResetRound = () => {
   choice.value = null
   computerChoice.value = null
   verdict.value = null
 }
 
-onMounted(()=>{
+onMounted(() => {
   LoadGame()
   window.addEventListener ('keypress', e => {
     if (e.key === 'r') {
@@ -84,7 +84,45 @@ onMounted(()=>{
 </script>
 
 <template>
-  <h1>Salut le monde !</h1>
+  <div class="bg-gray-700 text-white text-center min-h-screen flex flex-col">
+    <header class="container mx-auto p-6">
+      <h1 class="text-4x1 font-bold">Pierre, Feuille, Ciseaux</h1>
+    </header>
+    <main class="container mx-auto p-6 flex-1">
+      <div v-if="choice === null" class="flex items-center justify-center -mx-6">
+
+        <button @click="play('rock')" class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-blue-500">
+        <img src="./assets/hand-closed-fist-outline-svgrepo-com.svg" alt="Rock" class="w-full">
+        </button>
+
+        <button @click="play('paper')" class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-green-500">
+        <img src="./assets/hand-svgrepo-com.svg" alt="Paper" class="w-full">
+        </button>
+
+        <button @click="play('scissors')" class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-yellow-500">
+        <img src="./assets/scissors-svgrepo-com.svg" alt="scissors" class="w-full">
+        </button>
+      </div>
+
+      <div v-else>
+        <div class="text-3x1 mb-4">
+          You picked <span class="text-blue-500">{{ choice }}</span>
+        </div>
+
+        <div class="text-3x1 mb-4">
+          The computer picked <span class="text-green-500">{{ computerChoice }}</span>
+        </div>
+
+        <div class="text-6x1 mb-12">{{ verdict }}</div>
+        <button @click="ResetRound" class="bg-blue-500 text-lg py-2 px-4">Reset</button>
+      </div>
+
+      <div class="mt-12 text3x1 mb-4">{{ wins }} : {{ drawns }} : {{ losses }}</div>
+      <div class="text"></div>
+
+
+    </main>
+  </div>
 
 </template>
 
